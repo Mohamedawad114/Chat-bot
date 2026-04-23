1603394b9a562a65023e04ef9ec198b395f57d6c
Mohamedawad114 <mohamedahmedawad180@gmail.com>
2026-04-23 21:04:04 +0200

    change mail service

--- src/common/Utils/services/mailService/mail.module.ts
+++ src/common/Utils/services/mailService/mail.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { EmailServices } from './mail.service';
 import { HashingService } from '../../Hashing/hash.service';
 
 @Module({
-  imports: [
-    ConfigModule,
-    MailerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        transport: {
-          host: 'smtp.gmail.com',
-          port: 587,
-          family: 4,
-          secure: false,
-          auth: {
-            user: configService.get<string>('APP_GMAIL'),
-            pass: configService.get<string>('APP_PASSWORD'),
-          },
-        },
-      }),
-    }),
-  ],
+  imports: [ConfigModule],
   providers: [EmailServices, HashingService],
   exports: [EmailServices],
 })
